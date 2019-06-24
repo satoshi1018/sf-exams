5c2b86aa40a2ee201bbdc2f3d8e6668b6a181c40
姫谷聡 <satoshi19921117505@gmail.com>
2019-06-24 13:11:21 +0900

    再試験　提出②

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
-    @post_category = PostCategory.find(params[:id])
+    @post_categories = @post.post_categories
   end
 
   def new
